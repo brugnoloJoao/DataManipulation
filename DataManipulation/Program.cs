46ce58dd08dfb318508b413f6c079db473cf40f5
brugnoloJoao <brugnolojoao.31@gmail.com>
2026-04-25 12:18:57 -0300

    Introdução a Coleções - Initial commit com gitignore

---
+++ DataManipulation/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+
+var diasSemana = new string[]
+{
+    "Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"
+};
+
+/* O ArrayList era utilizado antigamente mas ele não restringe
+ o tipo de um elemento em uma coleção, então muitas vezes era necessário 
+verificar o tipo do elemento, o que dava mais trabalho*/
+
+
+var carrinho = new List<Produto>()
+{
+    new Produto() {Nome="Arroz", Preco=32.60},
+    new Produto() {Nome="Feijão", Preco=8.90}
+};
+
+/* Coleções do tipo List como */
+class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
