a62f993471ae82ad250c253c555cbadcfa251a62
brugnoloJoao <brugnolojoao.31@gmail.com>
2026-04-25 12:24:52 -0300

    Introdução a Coleções - Contiunuando Anotações

--- DataManipulation/Program.cs
+++ DataManipulation/Program.cs
@@ -6,18 +6,22 @@ var diasSemana = new string[]
     "Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"
 };
 
-/* O ArrayList era utilizado antigamente mas ele não restringe
+/* O ArrayList era utilizado antigamente, mas ele não restringe
  o tipo de um elemento em uma coleção, então muitas vezes era necessário 
-verificar o tipo do elemento, o que dava mais trabalho*/
+verificar o tipo do elemento antes de manipular ele, o que dava mais trabalho. 
+PS: a coleção acima não é uma ArrayList() ela é só um Array*/
 
 
 var carrinho = new List<Produto>()
 {
     new Produto() {Nome="Arroz", Preco=32.60},
     new Produto() {Nome="Feijão", Preco=8.90}
+    //"Quarta" -> Se fosse um ArrayList eu conseguiria fazer isso
 };
 
-/* Coleções do tipo List como */
+/* Coleções do tipo List, como carrinho, são mais utilizadas em relação a coleções
+ do tipo Array, porque o Array é uma coleção que você precisa manipular 
+manualmente o seu tamanho*/
 class Produto
 {
     public string Nome { get; set; }
